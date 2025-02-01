dffb0a09d135df3bb30bf3697895a49ac3929e0c
AleksGin <alexanderginin@icloud.com>
2025-02-02 02:35:23 +0300

    add Dog and Breed serializers

---
+++ dog_site/dogs/serializers.py
@@ -0,0 +1,39 @@
+from rest_framework import serializers
+from rest_framework.serializers import ModelSerializer
+
+from .models import (
+    Breed,
+    Dog,
+)
+
+
+class DogSerializer(ModelSerializer):
+    breed_avg_age = serializers.FloatField(read_only=True)
+    same_breed_count = serializers.IntegerField(read_only=True)
+    breed = serializers.SlugRelatedField(
+        queryset=Breed.objects.all(),
+        slug_field="name",
+    )
+
+    class Meta:
+        model = Dog
+        fields = [
+            "id",
+            "name",
+            "age",
+            "breed",
+            "breed_avg_age",
+            "same_breed_count",
+            "gender",
+            "color",
+            "favorite_food",
+            "favorite_toy",
+        ]
+
+
+class BreedSerializer(ModelSerializer):
+    dogs_breed_count = serializers.IntegerField(read_only=True)
+    class Meta:
+        model = Breed
+        fields = "__all__"
+        
