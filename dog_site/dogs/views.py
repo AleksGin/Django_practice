f780c10c235e9a83cf2fd0146342ac2aea58dfa0
AleksGin <alexanderginin@icloud.com>
2025-02-02 02:36:45 +0300

    add DogViewSet and BreedViewSet

--- dog_site/dogs/views.py
+++ dog_site/dogs/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from django.db.models import Avg, Count, F, OuterRef, Q, Subquery
+from django.db.models.functions import Coalesce, Round
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from .models import (
+    Breed,
+    Dog,
+)
+from .serializers import (
+    BreedSerializer,
+    DogSerializer,
+)
+
+
+class DogViewSet(ModelViewSet):
+    queryset = Dog.objects.annotate(
+        breed_avg_age=Round(
+            Coalesce(
+                Subquery(
+                    Dog.objects.filter(breed=OuterRef("breed"))
+                    .values("breed")
+                    .annotate(avg_age=Avg("age"))
+                    .values("avg_age")[:1]
+                ),
+                0.0,
+            ),
+            1,
+        ),
+        same_breed_count=Count(
+            "breed__dogs",
+            filter=Q(breed__dogs__breed=F("breed")),
+            distinct=True,
+        ),
+    )
+    serializer_class = DogSerializer
+
+
+class BreedViewSet(ModelViewSet):
+    queryset = Breed.objects.annotate(
+        dogs_breed_count=Count(
+            "dogs",
+            distinct=True,
+        )
+    )
+    serializer_class = BreedSerializer
