04ece1cd206b3a5adbfb74aa246daaac1997c676
AleksGin <alexanderginin@icloud.com>
2025-02-04 16:05:09 +0300

    add documenation to serializers.py

--- dog_site/dogs/serializers.py
+++ dog_site/dogs/serializers.py
@@ -8,6 +8,18 @@ from .models import (
 
 
 class DogSerializer(ModelSerializer):
+    """Сериализатор для модели Dog.
+
+    Добавляет вычисляемые поля:
+    - breed_avg_age: средний возраст собак этой породы.
+    - same_breed_count: количество собак той же породы.
+
+    Attributes:
+        breed_avg_age (FloatField): Средний возраст породы.
+        same_breed_count (IntegerField): Количество собак породы.
+        breed (SlugRelatedField): Связь с породой через имя (slug).
+    """
+
     breed_avg_age = serializers.FloatField(read_only=True)
     same_breed_count = serializers.IntegerField(read_only=True)
     breed = serializers.SlugRelatedField(
@@ -32,6 +44,15 @@ class DogSerializer(ModelSerializer):
 
 
 class BreedSerializer(ModelSerializer):
+    """Сериализатор для модели Breed.
+
+    Добавляет вычисляемое поле:
+    - dogs_breed_count: количество собак этой породы.
+
+    Attributes:
+        dogs_breed_count (IntegerField): Количество собак породы.
+    """
+
     dogs_breed_count = serializers.IntegerField(read_only=True)
 
     class Meta:
