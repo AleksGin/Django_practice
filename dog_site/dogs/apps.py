19590c47d5a0fb715678ff3bdc4f6b09048aa406
AleksGin <alexanderginin@icloud.com>
2025-02-04 16:04:47 +0300

    add documenation to

--- dog_site/dogs/apps.py
+++ dog_site/dogs/apps.py
@@ -2,5 +2,11 @@ from django.apps import AppConfig
 
 
 class DogsConfig(AppConfig):
+    """Конфигурация приложения Dogs.
+
+    Attributes:
+        default_auto_field (str): Тип поля для автоматического создания первичного ключа.
+        name (str): Имя приложения.
+    """
     default_auto_field = "django.db.models.BigAutoField"
     name = "dogs"
